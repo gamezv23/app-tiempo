0a5111d49f906d2916f459062f9a6f685bbbd894
Jorge Gamez <zorro8815@gmail.com>
2022-04-18 15:26:34 -0400

    inyectamos servicio de tiempo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TiempoService } from './services/tiempo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-time';
+
+  constructor(private servicio_de_tiempo:TiempoService){ }
 
 buscar(nombreBuscar:any){
   console.log(nombreBuscar.value)

ede4ab64045feaabc2f01312a8acd0cadf0377f7
Jorge Gamez <zorro8815@gmail.com>
2022-04-18 15:20:56 -0400

    creando get para obtener clima por ciudad

--- src/app/services/tiempo.service.ts
+++ src/app/services/tiempo.service.ts
@@ -7,4 +7,10 @@ import { Injectable } from '@angular/core';
 export class TiempoService {
 
   constructor(private http:HttpClient) { }
+
+obtenerTiempo(ciudad:string){
+
+  return this.http.get(`http://api.weatherapi.com/v1/forecast.json?key=0d1a024777b14a9993e150258222903&q=${ciudad}&days=3`)
+}
+
 }

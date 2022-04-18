b482aa34d47a8e75d7c81742d280b843a998f3cc
Jorge Gamez <zorro8815@gmail.com>
2022-04-18 15:41:29 -0400

    array de pronostico en na variable para iterarlo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,17 @@ import { TiempoService } from './services/tiempo.service';
 export class AppComponent {
   title = 'angular-time';
 
+pronostico:any;
+
   constructor(private servicio_de_tiempo:TiempoService){ }
 
 buscar(nombreBuscar:any){
   console.log(nombreBuscar.value)
   this.servicio_de_tiempo.obtenerTiempo(nombreBuscar.value)
-  .subscribe((datos)=>{
+  .subscribe((datos:any)=>{
     console.log(datos)
+    this.pronostico=datos.forecast.forecastday;
+    console.log(this.pronostico)
   })
 
 }
